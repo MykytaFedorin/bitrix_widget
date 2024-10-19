ac1189f61e090ea6d1a3bb548db04868c80027e3
Nikita Fedorin <xfedorin@stuba.sk>
2024-10-19 15:17:31 +0200

    php download file

--- backend/php/src/routes.php
+++ backend/php/src/routes.php
@@ -5,6 +5,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
+    $app->add(function (Request $request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    });
+
+    $app->options('/{routes:.+}', function (Response $response) {
+        return $response;
+    });
+
     $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
         $name = $args['name'];
         $response->getBody()->write("Hello, $name");
@@ -15,6 +27,29 @@ return function (App $app) {
         $data = $request->getParsedBody();
         $response->getBody()->write(json_encode($data));
         return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    $app->post('/cv/generate', function (Request $request, Response $response) {
+        if ($request->getUploadedFiles()) {
+            $uploadedFiles = $request->getUploadedFiles();
+            $file = $uploadedFiles['file'] ?? null;
+
+            if ($file && $file->getError() === UPLOAD_ERR_OK) {
+                $uploadFilePath = __DIR__ . '/' . $file->getClientFilename();
+
+                // Перемещаем загруженный файл
+                $file->moveTo($uploadFilePath);
+                
+                $response->getBody()->write(json_encode(['message' => 'File uploaded successfully']));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+            } else {
+                $response->getBody()->write(json_encode(['message' => 'File upload failed']));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+            }
+        } else {
+            $response->getBody()->write(json_encode(['message' => 'No file uploaded']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+        }
     });
 };
 
--- frontend/src/SendButton.js
+++ frontend/src/SendButton.js
@@ -1,15 +1,17 @@
 function SendButton() {
-    const url = process.env.REACT_APP_PYTHON_API_URL
-    const upload_url = url+"/cover_letter/generate"; 
+    const python_url = process.env.REACT_APP_PYTHON_API_URL;
+    const php_url = process.env.REACT_APP_PHP_API_URL;
+    const letter_url = python_url+"/cover_letter/generate"; 
+    const cv_url = php_url+"/cv/generate"; 
     const sendFile = () => {
         const formData = new FormData();
         const messageArea = document.getElementById("messageArea");
         const fileInput = document.querySelector('input[type="file"]');
         const textInput = document.querySelector('input[type="text"]');
-        if (fileInput.files.length > 0) {
+        if (fileInput.files.length > 0 && textInput.value !== "") {
             formData.append('file', fileInput.files[0]); 
             formData.append('description', textInput.value); 
-            fetch(upload_url, {
+            fetch(letter_url, {
                 method: 'POST',
                 body: formData,
                 headers: {
@@ -23,7 +25,26 @@ function SendButton() {
             })
             .then(data => messageArea.innerHTML = data)
             .catch((error) => console.error('Error:', error));
-        } else {
+        } 
+        else if(fileInput.files.length){
+            formData.append('file', fileInput.files[0]); 
+            fetch(cv_url, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                },
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => console.log())
+            .catch((error) => console.error('Error:', error));
+            
+        }
+        else {
             console.log("File not uploaded");
         }
     };
